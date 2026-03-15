4693bde05e62dbca797eda2e57043e5f3bb63e8e
Lukasz <lukasz.chudziak.14@gmail.com>
2026-03-15 12:39:29 +0100

    Changed .env varible and references, added healthcheck on database before app start

--- db.py
+++ db.py
@@ -7,9 +7,9 @@ load_dotenv()
 def get_connection():
     return psycopg2.connect(
         host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
         port=os.getenv("DB_PORT")
     )
 
